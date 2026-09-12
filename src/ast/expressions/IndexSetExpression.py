2785f5faccba716641fda383568fa923094e07e3
SharkBlocks00 <sharkblocks00@gmail.com>
2026-09-12 19:51:03 +0100

    Fixed bug where augment assignment operators would be double evaluated

--- src/ast/expressions/IndexSetExpression.py
+++ src/ast/expressions/IndexSetExpression.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
+
+from src.ast.expressions.BinaryExpression import BinaryExpression
+from src.tokens.Token import Token
 
 if TYPE_CHECKING:
     from src.runtime.Interpreter import Interpreter
@@ -10,16 +13,23 @@ from src.runtime.Environment import Environment
 from src.runtime.objects.FalxValue import FalxValue
 
 class IndexSetExpression(Expression):
-    def __init__(self, location: SourceLocation, obj: Expression, index: Expression, value: Expression) -> None:
+    def __init__(self, location: SourceLocation, obj: Expression, index: Expression, value: Expression, operator: Optional[Token] = None) -> None:
         super().__init__(location)
         self.obj = obj
         self.index = index
         self.value = value
+        self.operator = operator
 
     def evaluate(self, interpreter: Interpreter, environment: Environment) -> FalxValue:
         falxObject: FalxValue = self.obj.evaluate(interpreter, environment)
         evaluatedIndex: FalxValue = self.index.evaluate(interpreter, environment)
-        evaluatedValue: FalxValue = self.value.evaluate(interpreter, environment)
+
+        if self.operator is not None:
+            currentValue: FalxValue = falxObject.index(evaluatedIndex)
+            rhsValue: FalxValue = self.value.evaluate(interpreter, environment)
+            evaluatedValue: FalxValue = BinaryExpression.applyOperator(self.operator.tokenKind, currentValue, rhsValue)
+        else:
+            evaluatedValue: FalxValue = self.value.evaluate(interpreter, environment)
 
         falxObject.indexAssign(evaluatedIndex, evaluatedValue)
         return evaluatedValue
--- src/ast/expressions/SetExpression.py
+++ src/ast/expressions/SetExpression.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
+
+from src.ast.expressions.BinaryExpression import BinaryExpression
+from src.tokens.Token import Token
 
 if TYPE_CHECKING:
     from src.runtime.Interpreter import Interpreter
@@ -11,16 +14,25 @@ from src.runtime.objects.FalxValue import FalxValue
 
 
 class SetExpression(Expression):
-    def __init__(self, location: SourceLocation, obj: Expression, _property: str, value: Expression):
+    def __init__(self, location: SourceLocation, obj: Expression, _property: str, value: Expression, operator: Optional[Token] = None):
         super().__init__(location)
         self.obj = obj
         self.property = _property
         self.value = value
+        # None for plain obj.prop = value BUT: set to base operator
+        # for compound assignment, eg TokenKind.PLUS for +=
+        self.operator = operator
 
 
     def evaluate(self, interpreter: Interpreter, environment: Environment) -> FalxValue:
         falxObject: FalxValue = self.obj.evaluate(interpreter, environment)
-        evaluatedValue: FalxValue = self.value.evaluate(interpreter, environment)
+
+        if self.operator is not None:
+            currentValue: FalxValue = falxObject.get(self.property)
+            rhsValue: FalxValue = self.value.evaluate(interpreter, environment)
+            evaluatedValue: FalxValue = BinaryExpression.applyOperator(self.operator.tokenKind, currentValue, rhsValue)
+        else:
+            evaluatedValue: FalxValue = self.value.evaluate(interpreter, environment)
 
         falxObject.set(self.property, evaluatedValue)
         return evaluatedValue
