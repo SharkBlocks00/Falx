d86d7a49890345cc5f34d4a57871d8fc9bb5b252
SharkBlocks00 <sharkblocks00@gmail.com>
2026-09-20 20:33:52 +0100

    Upgraded ArgumentHandler to use argparse instead of just match statements

--- src/ArgumentHandler.py
+++ src/ArgumentHandler.py
@@ -1,33 +1,44 @@
-import sys
+import argparse
 from pathlib import Path
 
-from src.TestRunner import TestRunner
-from src.diagnostics.exceptions.cli.MissingFilenameException import MissingFilenameException
-from src.diagnostics.exceptions.cli.InvalidFileTypeException import InvalidFileTypeException
-
-
-testRunner: TestRunner = TestRunner()
 
 class ArgumentHandler:
     def __init__(self, arguments: list[str]):
-        self.arguments: list[str] = arguments
+        self.arguments = arguments
 
-    def handleArguments(self) -> Path | None:
-        for i, argument in enumerate(self.arguments):
-            match argument:
-                case "--version" | "-v":
-                    print("Falx v0.2.0")
-                    sys.exit(0)
-                case "--file" | "-f" | "run":
-                    if i + 1 >= len(self.arguments):
-                        raise MissingFilenameException(self.arguments[i])
+    def handleArguments(self) -> argparse.Namespace:
+        parser = argparse.ArgumentParser(
+            prog="falx",
+            description="The Falx programming language"
+        )
 
-                    path: Path = Path(self.arguments[i + 1])
+        parser.add_argument(
+            "--version",
+            "-v",
+            action="version",
+            version="Falx v0.2.0"
+        )
 
-                    if path.suffix != ".flx":
-                        raise InvalidFileTypeException(self.arguments[i])
-                    return path
+        subparsers = parser.add_subparsers(dest="command")
 
-                case "--test" | "-t":
-                    testRunner.runAll()
-        return None
+        runParser = subparsers.add_parser(
+            "run",
+            aliases=["--file", "-f"]
+        )
+
+        runParser.add_argument(
+            "file",
+            type=Path
+        )
+
+        testParser = subparsers.add_parser(
+            "test",
+            aliases=["-t"]
+        )
+
+        testParser.add_argument(
+            "--update-snapshots",
+            action="store_true"
+        )
+
+        return parser.parse_args(self.arguments[1:])
