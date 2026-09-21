c4600fc87169e0a391056ef3056526e878797c9d
SharkBlocks00 <sharkblocks00@gmail.com>
2026-09-21 18:28:16 +0100

    Fixed two or more dimensional arrays being formatted as flat arrays

--- src/runtime/objects/FalxString.py
+++ src/runtime/objects/FalxString.py
@@ -63,7 +63,7 @@ class FalxString(FalxValue, FalxIterable):
         return self.value
 
     def __repr__(self):
-        return self.value
+        return f'"{self.value}"'
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, FalxString) and self.value == other.value
