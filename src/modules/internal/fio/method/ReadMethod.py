dfe4848fc6c379b6b0b402089ad4421b34857855
SharkBlocks00 <sharkblocks00@gmail.com>
2026-09-26 17:26:03 +0100

    Changed fio.read method to accept just one argument

--- src/modules/internal/fio/method/ReadMethod.py
+++ src/modules/internal/fio/method/ReadMethod.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from src.diagnostics.exceptions.runtime.ExpectedValueException import ExpectedValueException
+from src.diagnostics.exceptions.runtime.methods.InvalidArgumentCountException import InvalidArgumentCountException
 from src.runtime.methods.common.BoundNativeFunction import BoundNativeFunction
 from src.runtime.objects.FalxFile import FalxFile
 from src.runtime.objects.FalxString import FalxString
@@ -19,14 +20,24 @@ class ReadMethod(BoundNativeFunction):
         return 2
 
     def isStrict(self) -> bool:
-        return True
+        return False
 
     def call(self, interpreter: Interpreter, arguments: list[FalxValue]) -> FalxValue:
+
+        if len(arguments) <= 0 or len(arguments) > 2:
+            raise InvalidArgumentCountException(len(arguments), 2)
+
         file = arguments[0]
 
         if not isinstance(file, FalxFile):
             raise ExpectedValueException(f"Expected file, got '{file.getTypeName()}'")
-        size = arguments[1].asInt()
+
+        size: int | None
+        try:
+            size = arguments[1].asInt()
+        except IndexError:
+            size = None
+
         return FalxString(file.file.read(size))
 
     def __eq__(self, other: object) -> bool:
